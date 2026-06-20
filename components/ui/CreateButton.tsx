2fca6d165144a5a717f45586bbf7bc898402b142
coffee4urhead <dark1982soul@gmail.com>
2026-06-20 18:54:42 +0300

    added some more of the create button

---
+++ components/ui/CreateButton.tsx
@@ -0,0 +1,13 @@
+import { StyleSheet, View } from 'react-native';
+
+export default function CreateButton() {
+    return (
+        <View style={}>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+  
+});
